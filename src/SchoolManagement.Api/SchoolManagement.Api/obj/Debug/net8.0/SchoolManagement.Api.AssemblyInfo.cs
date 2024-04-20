95c9d0050ce8591dcd4df1f91848c3a8167fae00
quan.doan <dvquan210502@gmail.com>
2024-04-20 08:33:28 +0700

    update

--- src/SchoolManagement.Api/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
+++ src/SchoolManagement.Api/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37081f8f37a05d1f02d024325a9f8b25ce15d352")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
